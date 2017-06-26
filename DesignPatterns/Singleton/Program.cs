523de7d1add1e1a8e41f12357f4c7d88e941ad07
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2017-06-26 17:50:39 +0200

    Added Singleton Pattern.

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton firstInstance = Singleton.GetInstance();
+            Console.WriteLine($"First instance ID: {firstInstance.GetHashCode()}");
+
+            Singleton secondInstance = Singleton.GetInstance();
+            Console.WriteLine($"Second instance ID: {secondInstance.GetHashCode()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+        private static readonly object obj = new object();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (obj)
+                {
+                    _instance = new Singleton();
+                }
+            }
+
+            return _instance;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Singleton instance is writting...");
+        }
+    }
+}
