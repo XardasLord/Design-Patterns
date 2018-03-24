c598288032eb369b8486452472857fdfef1f5980
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-03-24 12:13:01 +0100

    Added Singleton - Monostate

---
+++ DesignPatterns/Singleton - Monostate/CEO.cs
@@ -0,0 +1,29 @@
+﻿namespace Singleton___Monostate
+{
+    public class CEO
+    {
+        // These fields are static, and the properties bellow are non-static,
+        // so they point always to the same fields, no mather from which CEO object it is called,
+        // so all CEO object treat like a Singleton
+
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+}
---
+++ DesignPatterns/Singleton - Monostate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton___Monostate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // All CEO objects points to the same fields (cause they are static)
+            // To see more information see the CEO class
+
+            var ceo = new CEO();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new CEO();
+            Console.WriteLine(ceo2); // Adam Smith with age 55
+
+            Console.ReadLine();
+        }
+    }
+}
