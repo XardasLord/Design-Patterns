88c294980ee06db044526d62a4b2d87dd75e3cf2
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-03-26 20:18:21 +0200

    Added Bridge Pattern

---
+++ DesignPatterns/Bridge/Program.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bridge pattern allows you to connect different abstraction together
+
+            var cb = new ContainerBuilder();
+            cb.RegisterType<VectorRenderer>().As<IRenderer>()
+                .SingleInstance();
+            cb.Register((c, p) => new Circle(c.Resolve<IRenderer>(), p.Positional<float>(0)));
+
+            using(var c = cb.Build())
+            {
+                var circle = c.Resolve<Circle>(new PositionalParameter(0, 5.0f));
+                circle.Draw();
+                circle.Resize(2);
+                circle.Draw();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatterns/Bridge/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        public Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+}
