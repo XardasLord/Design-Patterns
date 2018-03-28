2baecaca31fd39d083c0fa92a82ea474fe317c22
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-03-28 18:25:14 +0200

    Added Decorator Pattern (Custom String Builder example)

---
+++ DesignPatterns/Decorator - Custom String Builder/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace Decorator___Custom_String_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Decorator Pattern using by creating our own CodeBuilder which not really directly inherit from StringBuilder but uses the decorator
+            // See the CodeBuilder class
+
+            var cb = new CodeBuilder();
+            cb.AppendLine("class Foo")
+              .AppendLine("{")
+              .AppendLine("}");
+            WriteLine(cb);
+
+            ReadLine();
+        }
+    }
+}
