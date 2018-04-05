05d0dae04c2d16736248c0a7fe66a553fc0bbf28
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-04-05 23:17:48 +0200

    Added Flyweight Pattern (Text Formatting)

---
+++ DesignPatterns/Flyweight - Text Formatting/BetterFormattedText.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight___Text_Formatting
+{
+    class BetterFormattedText
+    {
+        private readonly string plainText;
+        private List<TextRange> formatting = new List<TextRange>();
+
+        public BetterFormattedText(string plainText)
+        {
+            this.plainText = plainText;
+        }
+
+        public TextRange GetRange(int start, int end)
+        {
+            var range = new TextRange { Start = start, End = end };
+            formatting.Add(range);
+            return range;
+        }
+
+        public class TextRange
+        {
+            public int Start, End;
+            public bool Capitalize, Bold, Italic;
+
+            public bool Covers(int position)
+            {
+                return position >= Start && position <= End;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                var c = plainText[i];
+                foreach (var range in formatting)
+                    if (range.Covers(i) && range.Capitalize)
+                        c = char.ToUpper(c);
+
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ DesignPatterns/Flyweight - Text Formatting/FormattedText.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Flyweight___Text_Formatting
+{
+    public class FormattedText
+    {
+        private readonly string plainText;
+        private bool[] capitalize;
+
+        public FormattedText(string plainText)
+        {
+            this.plainText = plainText;
+            capitalize = new bool[plainText.Length];
+        }
+
+        public void Capitalize(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                capitalize[i] = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                var c = plainText[i];
+                sb.Append(capitalize[i] ? char.ToUpper(c) : c);
+            }
+            return sb.ToString();
+        }
+    }
+}
