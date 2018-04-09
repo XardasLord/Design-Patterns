a5765500d5f19507c9e4c0c4319b60b436c76ae3
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-04-09 20:41:25 +0200

    Added Information aboud Static Strategy in the Strategy Pattern example.

--- DesignPatterns/Strategy - Dynamic/Program.cs
+++ DesignPatterns/Strategy - Dynamic/Program.cs
@@ -14,6 +14,7 @@ namespace Strategy___Dynamic
         {
             var list = new[] { "foo", "bar", "baz", "var" };
 
+            // This is the dynamic strategy (at runtime)
             var tp = new TextProcessor();
             tp.SetOutputFormat(OutputFormat.Markdown);
             tp.AppendList(list);
@@ -23,6 +24,13 @@ namespace Strategy___Dynamic
             tp.SetOutputFormat(OutputFormat.Html);
             tp.AppendList(list);
             Console.WriteLine(tp);
+
+            // You can also delegate it to the Dependency Injection Container like this (good way)
+            // This is how it's typically done in the real world:
+            // It is defined at compile time, not at runtime:
+            //
+            // cb.Register<MarkdownListStrategy>().As<IListStrategy>();
+
 
 
             Console.ReadLine();
